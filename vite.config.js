a69693ffe3842dfe97a68259c9a2b2c6b8ecad44
Ganesh Ban <banganesh98@gmail.com>
2026-05-13 15:45:59 -0500

    static push

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/banbandhu/",
-  server: {
-    allowedHosts: [".ganeshban.com.np"]
-  },
+  // base: "/banbandhu/",
+  // server: {
+  //   allowedHosts: [".ganeshban.com.np"]
+  // },
   plugins: [react()],
 });
